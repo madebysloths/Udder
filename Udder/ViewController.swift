8c8c3f9d9c1b09f9fc1702f5dc7fe5ed9f7de538
James <madebysloths@gmail.com>
2019-07-08 19:27:51 -0500

    Add labels

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by James on 7/8/19.
+//  Copyright © 2019 Made By Sloths. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
